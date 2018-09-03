df378caef5d282fafc6c48f4d43bcf138d479b72
Risato_Desktop <rickypsatomi@gmail.com>
2018-09-02 18:06:50 -0700

    Descriptive Update

--- app.R
+++ app.R
@@ -1,48 +1,48 @@
 library(shiny)
 library(readxl)
 library(caTools)
-library(dplyr)
+library(dplyr, warn.conflicts = FALSE)
 library(DT)
 
 
 
 runApp(list(
   ui=fluidPage(
-    titlePanel("Pond Calculator"
-    ),
+    titlePanel("Pond Calculator"),
+    
+    p("This is a Pond Calculator developed to assist with stocking pond volumes. Enter your depths and associated surface area into the calculator below to generate a pond curve for your property. To save your data, download your table as a csv or xlsx file after you have generated the table."),
     
     sidebarPanel(
-      h3("Step 1", align="center"),
+      h3("Step 1", align="Left"),
+      fileInput('excelupload', 'Upload Existing Pond Table', accept = c(".xlsx")),
       
-      fileInput('excelupload', 'Use Existing Pond Table', accept = c(".xlsx")),
-      
-      h3("or",align="center"),
+      h3("Step 2", align="Left"),
       numericInput("PondDepth", "Pond Depth (Feet)",NULL),
       numericInput("PondSurface", "Pond Surface Area (Square-Feet)",NULL),
       actionButton("update", "Add new Entry"),
       
       ## Islands need to be added in the spreadsheet later
-      h3("Step 2", align="center"),
+      h3("Step 3", align="Left"),
       p("Under Development: Add Islands"),
       # numericInput("text1", "Add Island Depth",NULL),
       # numericInput("text2", "Add IslandArea",NULL),
       # actionButton("update", "Update Table"),
       
-      h3("Step 3", align="center"),
-      p("Select incorrect rows from the table on the right and click on the button below"),
+      h3("Step 4", align="Left"),
+      p("Under Development Select incorrect rows from the table on the right and click on the button below"),
       actionButton("deleteRows", "Remove Selected Rows"),
       
-      h3("Step 4", align = "center"),
+      h3("Step 5", align = "Left"),
       actionButton("calculate", "Calculate Pond Volume")
+      
       # Maybe add this visualization: https://shiny.rstudio.com/gallery/plot-interaction-exclude.html
       # Need to talk to range guys to see what they want. lm plot may be overkill
       
     ),
     
     mainPanel(
-      p("Pond Table"),
       DT::dataTableOutput('PondMeasurement'),
-      textOutput("selected_var")
+      span(textOutput("selected_var"), style="color:red")
     )
   ),
   
@@ -52,7 +52,7 @@ runApp(list(
     
     # Create empty reactive values to receive manually added data    
     values <- reactiveValues()
-    values$df <- data.frame(Depth = NULL, Area = NULL)
+    values$df <- data.frame(Depth = NA, Area = NA)
     Result<-0.0
     
     # Upload XLSX into R Shiny App
@@ -77,9 +77,9 @@ runApp(list(
     
     observeEvent(input$deleteRows,{
       
-      if (!is.null(input$table1_rows_selected)) {
+      if (!is.null(input$PondMeasurement_rows_selected)) {
         
-        values$df <- values$df[-as.numeric(input$table1_rows_selected),]
+        values$df <- values$df[-as.numeric(input$PondMeasurement_rows_selected),]
       }
     })
     
@@ -96,17 +96,13 @@ runApp(list(
     output$PondMeasurement <- DT::renderDataTable(
       DT::datatable(values$df, 
                     rownames = FALSE, 
-                    extensions = 'Buttons',
-                    
+                    extensions = 'Buttons', 
                     options = list(
                       paging = FALSE, 
                       searching = FALSE,
                       dom = 'Bfrtip',
-                      buttons = c('copy', 'csv', 'excel', 'pdf', 'print')
-                      
-                    )
-      )
-    )
+                      buttons = list('copy', 'csv', 'excel', 'pdf', 'print'))
+                    ))
     
     
     newEntry <- observe({
